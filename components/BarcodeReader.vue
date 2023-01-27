97e9b04a9c7802133dc46947f864a9434c1fbc5b
Apiwat Nongwa <>
2023-01-27 21:04:47 +0700

    test

---
+++ components/BarcodeReader.vue
@@ -0,0 +1,28 @@
+<template>
+    <div>
+        <StreamBarcodeReader @decode="onDecode" @loaded="onLoaded"></StreamBarcodeReader>
+        <hr />
+
+        <span>{{ barcode }}</span>
+    </div>
+</template>
+<script>
+import { StreamBarcodeReader } from 'vue-barcode-reader'
+export default {
+    components: {
+        StreamBarcodeReader,
+    },
+    data() {
+        return {
+            barcode: null
+        }
+    },
+    methods:{
+        onDecode(result){
+            // eslint-disable-next-line no-console
+            console.log(result);
+        },
+        onLoaded(){},
+    }
+}
+</script>
---
+++ pages/index.vue
@@ -0,0 +1,26 @@
+<template>
+  <v-row justify="center" align="center">
+    <v-col cols="12" sm="8" md="6">
+      <v-card>
+        <v-card-title class="headline">
+          Welcome to the Vuetify + Nuxt.js template
+        </v-card-title>
+        <v-card-text>
+          <client-only>
+          <barcode-reader />
+          </client-only>
+        </v-card-text>
+        <v-card-actions>
+          <v-spacer />
+          <v-btn
+            color="primary"
+            nuxt
+            to="/inspire"
+          >
+            Continue
+          </v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-col>
+  </v-row>
+</template>
